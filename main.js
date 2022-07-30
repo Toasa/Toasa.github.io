f54531bd66414189ca853b0241a429640bc39a40
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-07-30 23:43:16 +0900

    wip

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function buttonClick() {
     var nth = document.getElementById('nth').value
-    alert(nth);
 
     output = document.getElementById('output')
-    target.innerText = 'hello'
+    output.innerText = 'nth: ' + nth
 }
