bd62a72aa0dc75abcd474a73ef09a56cf7d47965
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-07-30 23:49:18 +0900

    wip

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ function buttonClick() {
     var nth = document.getElementById('nth').value
 
     output = document.getElementById('output')
-    output.innerText = 'nth: ' + nth
+    output.innerText = 'nth: ' + fibonacci(nth)
+}
+
+function fibonacci(n) {
+    if (n < 2)
+        return 1
+
+    return f(n-1) + f(n-2)
 }
